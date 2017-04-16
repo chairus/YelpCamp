0e1efd65de388df87cc10f92d25fea69cfe06f5a
Cyrus Villacampa <chairus06@gmail.com>
2017-04-16 13:22:01 +0000

    Update db url

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ var commentRoutes           = require("./routes/comments");
 var campgroundRoutes        = require("./routes/campgrounds");
 var indexRoutes             = require("./routes/index");
 
+console.log(process.env.DATABASEURL);
+
 // APP CONFIG;
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://cyrus:Mfdw-06@ds037617.mlab.com:37617/yelpcamp");
+// mongodb://localhost/yelp_camp
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://cyrus:Mfdw-06@ds037617.mlab.com:37617/yelpcamp");
 app.use(methodOverride("_method"));
 app.use(flash());
 app.use(bodyParser.urlencoded({extended: true}));
